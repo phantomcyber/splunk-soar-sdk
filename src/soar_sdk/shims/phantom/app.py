42af2ec96a42d240947ccc60127c1e4fe7befb7d
Scott Odle <sodle@splunk.com>
2025-04-08 09:23:19 -0600

    fix(shims): limit what is exported from shims.phantom.app

--- src/soar_sdk/shims/phantom/app.py
+++ src/soar_sdk/shims/phantom/app.py
@@ -1,5 +1,8 @@
 try:
-    from phantom.app import *  # noqa: F403
+    from phantom.app import APP_SUCCESS, APP_ERROR
 except ImportError:
     APP_SUCCESS = True
     APP_ERROR = False
+
+
+__all__ = ["APP_SUCCESS", "APP_ERROR"]
