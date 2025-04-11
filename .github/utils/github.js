5e94a2e4436ef073e9eca4ebe693254ce5a4e48b
Scott Odle <sodle@splunk.com>
2025-04-11 16:05:46 -0600

    fix(release): await promise when getting release id

--- .github/utils/github.js
+++ .github/utils/github.js
@@ -1,14 +1,18 @@
 const fs = require("fs");
 
-function uploadReleaseArtifacts({ github, context }) {
+async function uploadReleaseArtifacts({ github, context }) {
   const tag = fs.readFileSync("release_version.txt").toString().trim();
   const { owner, repo } = context.repo;
 
   const artifacts = fs
     .readdirSync("dist")
     .filter((f) => f.endsWith(".whl") || f.endsWith(".tar.gz"));
-  const release_id = github.rest.repos.getReleaseByTag({ owner, repo, tag })
-    .data.id;
+  const getRelease = await github.rest.repos.getReleaseByTag({
+    owner,
+    repo,
+    tag,
+  });
+  const release_id = getRelease.data.id;
 
   artifacts.forEach((name) => {
     const data = fs.readFileSync(`dist/${name}`);
