ca2f953187a9b1ce3e593145d2c760b86c1f6969
Scott Odle <sodle@splunk.com>
2025-04-16 09:05:03 -0600

    fix(ci): handle cases where semantic-release does not release a new version

--- .github/utils/github.js
+++ .github/utils/github.js
@@ -1,7 +1,19 @@
 const fs = require("fs");
 
+function getReleaseVersion() {
+  if (fs.existsSync("release_version.txt")) {
+    return fs.readFileSync("release_version.txt").toString().trim();
+  }
+  return null;
+}
+
 async function uploadReleaseArtifacts({ github, context }) {
-  const tag = fs.readFileSync("release_version.txt").toString().trim();
+  const tag = getReleaseVersion();
+  if (!tag) {
+    console.log("This change did not result in a release.");
+    return
+  }
+
   const { owner, repo } = context.repo;
 
   const artifacts = fs
@@ -27,15 +39,21 @@ async function uploadReleaseArtifacts({ github, context }) {
 }
 
 function commentReleaseNotes({ github, context }) {
-  const versionNumber = fs
-    .readFileSync("release_version.txt")
-    .toString()
-    .trim();
+  const versionNumber = getReleaseVersion();
+  if (!versionNumber) {
+    return github.rest.issues.createComment({
+      issue_number: context.issue.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      body: "Merging this PR will not result in a release.",
+    });
+  }
+
   const releaseNotes = fs.readFileSync("release_notes.txt").toString().trim();
 
   const commentBody = `Merging this PR will release \`${versionNumber}\` with the following release notes:\n\n${releaseNotes}`;
 
-  github.rest.issues.createComment({
+  return github.rest.issues.createComment({
     issue_number: context.issue.number,
     owner: context.repo.owner,
     repo: context.repo.repo,
