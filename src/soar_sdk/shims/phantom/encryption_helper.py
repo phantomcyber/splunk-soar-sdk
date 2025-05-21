e565ddbf04dad2e696607cab7e09d6c9a93d8b87
mnordby-splunk <mnordby@splunk.com>
2025-05-21 17:22:11 -0500

    fix: papp-36014 update shims

--- src/soar_sdk/shims/phantom/encryption_helper.py
+++ src/soar_sdk/shims/phantom/encryption_helper.py
@@ -1,5 +1,5 @@
 try:
-    from phantom import encryption_helper
+    from phantom.encryption_helper import EncryptionHelper
 
     _soar_is_available = True
 except ImportError:
@@ -10,23 +10,16 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING or not _soar_is_available:
     import base64
 
-    def encrypt(encrypt_input: str) -> str:
-        """Mock function to simulate the behavior of encryption_helper.encrypt."""
-        return base64.b64encode(encrypt_input.encode("utf-8")).decode("utf-8")
+    class EncryptionHelper:  # type: ignore[no-redef]
+        @staticmethod
+        def encrypt(plain: str, salt: str = "") -> str:
+            """Simulates the behavior of encryption_helper.encrypt."""
+            return base64.b64encode(plain.encode("utf-8")).decode("utf-8")
 
-    def decrypt(decrypt_input: str) -> str:
-        """Mock function to simulate the behavior of encryption_helper.decrypt."""
-        return base64.b64decode(decrypt_input.encode("utf-8")).decode("utf-8")
-
-
-def encrypt(encrypt_input: str) -> str:  # type: ignore[no-redef]
-    """Returns an encrypted string"""
-    return encryption_helper.encrypt(encrypt_input)
+        @staticmethod
+        def decrypt(cipher: str, salt: str = "") -> str:
+            """Simulate the behavior of encryption_helper.decrypt."""
+            return base64.b64decode(cipher.encode("utf-8")).decode("utf-8")
 
 
-def decrypt(decrypt_input: str) -> str:  # type: ignore[no-redef]
-    """Returns a decrypted string"""
-    return encryption_helper.decrypt(decrypt_input)
-
-
-__all__ = ["decrypt", "encrypt"]
+__all__ = ["EncryptionHelper"]
--- tests/test_encryption_helper.py
+++ tests/test_encryption_helper.py
@@ -1,34 +1,10 @@
-import sys
 import base64
-from unittest import mock
-
-
-def test_mocked_encryption_helper():
-    with mock.patch("phantom.encryption_helper") as mock_encryption_helper:
-        # Mock the encrypt and decrypt functions
-        mock_encryption_helper.encrypt.return_value = "encrypted_value"
-        mock_encryption_helper.decrypt.return_value = "decrypted_value"
-
-        # Import the encryption_helper shim
-        import soar_sdk.shims.phantom.encryption_helper as encryption_helper
-
-        encrypted_value = encryption_helper.encrypt("test_string")
-        decrypted_value = encryption_helper.decrypt("test_string")
-        assert encrypted_value == "encrypted_value"
-        assert decrypted_value == "decrypted_value"
-
-    # Check that the encrypt and decrypt functions were called
-    mock_encryption_helper.encrypt.assert_called_once_with("test_string")
-    mock_encryption_helper.decrypt.assert_called_once_with("test_string")
+from soar_sdk.shims.phantom.encryption_helper import (
+    EncryptionHelper as encryption_helper,
+)
 
 
 def test_encryption_helper_not_available():
-    # Simulate the absence of phantom.encryption_helper
-    sys.modules["phantom.encryption_helper"] = None
-
-    # Import the encryption_helper shim
-    import soar_sdk.shims.phantom.encryption_helper as encryption_helper
-
     # Check that the encrypt and decrypt functions are shimmed correctly
     assert encryption_helper.encrypt("test_string") == base64.b64encode(
         b"test_string"
