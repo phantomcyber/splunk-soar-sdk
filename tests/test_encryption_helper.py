5c4e81431bc03804b8f4d8c5eb7b20d72aa676c6
mnordby-splunk <mnordby@splunk.com>
2025-05-22 11:12:30 -0500

    fix: papp-36014 update mock test

--- tests/test_encryption_helper.py
+++ tests/test_encryption_helper.py
@@ -1,11 +1,13 @@
 import base64
-from soar_sdk.shims.phantom.encryption_helper import (
-    EncryptionHelper as encryption_helper,
-)
+from unittest import mock
 
 
 def test_encryption_helper_not_available():
-    # Check that the encrypt and decrypt functions are shimmed correctly
+    # Test the behavior when the EncryptionHelper is not available
+    from soar_sdk.shims.phantom.encryption_helper import (
+        EncryptionHelper as encryption_helper,
+    )
+
     assert encryption_helper.encrypt("test_string") == base64.b64encode(
         b"test_string"
     ).decode("utf-8")
@@ -14,3 +16,25 @@ def test_encryption_helper_not_available():
         encryption_helper.decrypt(encryption_helper.encrypt("test_string_"))
         == "test_string_"
     )
+
+
+def test_encryption_helper_phantom_available():
+    # Test the behavior when the EncryptionHelper is available
+    class MockEncryptionHelper:
+        @staticmethod
+        def encrypt(plain: str, salt: str = "") -> str:
+            return "mocked_encrypted"
+
+        @staticmethod
+        def decrypt(cipher: str, salt: str = "") -> str:
+            return "mocked_decrypted"
+
+    with mock.patch(
+        "soar_sdk.shims.phantom.encryption_helper.EncryptionHelper",
+        MockEncryptionHelper,
+    ):
+        # Re-import the class to pick up the mocked version
+        from soar_sdk.shims.phantom.encryption_helper import EncryptionHelper
+
+        assert EncryptionHelper.encrypt("test_string") == "mocked_encrypted"
+        assert EncryptionHelper.decrypt("test_string") == "mocked_decrypted"
