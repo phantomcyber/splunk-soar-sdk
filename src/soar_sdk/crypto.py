adfc38f2bd205997d5b7d1e25220008011cecba5
mnordby-splunk <mnordby@splunk.com>
2025-05-27 09:18:39 -0600

    chore: apply suggestions from code review
    Co-authored-by: phantom-jacob <43217172+phantom-jacob@users.noreply.github.com>

--- src/soar_sdk/crypto.py
+++ src/soar_sdk/crypto.py
@@ -1,17 +1,11 @@
 from soar_sdk.shims.phantom.encryption_helper import encryption_helper
 
 
-class Encryption:
-    """
-    Encryption class to handle encryption and decryption of data.
-    """
+def encrypt(plain: str) -> str:
+    # Encrypts the given plain text
+    return encryption_helper.encrypt(plain, "")
 
-    @staticmethod
-    def encrypt(plain: str) -> str:
-        # Encrypts the given plain text
-        return encryption_helper.encrypt(plain, "")
 
-    @staticmethod
-    def decrypt(cipher: str) -> str:
-        # Decrypts the given cipher text
-        return encryption_helper.decrypt(cipher, "")
+def decrypt(cipher: str) -> str:
+    # Decrypts the given cipher text
+    return encryption_helper.decrypt(cipher, "")
