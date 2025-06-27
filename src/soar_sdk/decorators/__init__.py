9a9661f1b6133640d68f9da1465b436e8b413960
Tapish Jain <tapishj@splunk.com>
2025-06-27 10:35:56 -0700

    fix: change test connectivity decorater class name so pytest stops complaining

--- src/soar_sdk/decorators/__init__.py
+++ src/soar_sdk/decorators/__init__.py
@@ -5,15 +5,15 @@ This module provides class-based decorators for SOAR app development.
 """
 
 from .action import ActionDecorator
-from .test_connectivity import TestConnectivityDecorator
+from .test_connectivity import ConnectivityTestDecorator
 from .view_handler import ViewHandlerDecorator
 from .on_poll import OnPollDecorator
 from .webhook import WebhookDecorator
 
 __all__ = [
     "ActionDecorator",
+    "ConnectivityTestDecorator",
     "OnPollDecorator",
-    "TestConnectivityDecorator",
     "ViewHandlerDecorator",
     "WebhookDecorator",
 ]
