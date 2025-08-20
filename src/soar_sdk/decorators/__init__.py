433865bcb727f4088fcdd8d8ad1cf95adc5b329f
Tapish Jain <tapishj@splunk.com>
2025-08-20 12:49:24 -0700

    feat: new generic action

--- src/soar_sdk/decorators/__init__.py
+++ src/soar_sdk/decorators/__init__.py
@@ -9,10 +9,12 @@ from .test_connectivity import ConnectivityTestDecorator
 from .view_handler import ViewHandlerDecorator
 from .on_poll import OnPollDecorator
 from .webhook import WebhookDecorator
+from .generic_action import GenericActionDecorator
 
 __all__ = [
     "ActionDecorator",
     "ConnectivityTestDecorator",
+    "GenericActionDecorator",
     "OnPollDecorator",
     "ViewHandlerDecorator",
     "WebhookDecorator",
