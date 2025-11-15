e95f6a311e26bd74c882feb89647649a73fdc661
Scott Odle <sodle@splunk.com>
2025-11-14 17:48:27 -0700

    fix: give the webhook app a separate product_name

--- tests/example_app_with_webhook/src/app.py
+++ tests/example_app_with_webhook/src/app.py
@@ -28,7 +28,7 @@ app = App(
     product_vendor="Splunk Inc.",
     logo="logo.svg",
     logo_dark="logo_dark.svg",
-    product_name="Example App",
+    product_name="Example App with Webhoo",
     publisher="Splunk Inc.",
     min_phantom_version="6.2.2.134",
 ).enable_webhooks()
